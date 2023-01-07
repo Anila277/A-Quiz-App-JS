fa29511f3b746c3f399895a7d2f48fa10fe1b482
Anila Nisar <raniyasr@gmail.com>
2023-01-06 21:50:29 -0500

    wip: finished most functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
@@ -6,6 +7,10 @@ const answerButtonsElement = document.getElementById('answer-buttons')
 let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     startButton.classList.add('hide')
@@ -34,16 +39,79 @@ function showQuestion(question) {
     })
 }
 
-function selectAnswer() {
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+            (answerButtonsElement.firstChild)
+    }
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
     {
-        question: 'What is 2 + 2',
+        question: 'What is 2 + 2?',
         answers: [
             { text: '4', correct: true },
-            { text: '22', correct: false }
+            { text: '22', correct: false },
+            { text: '42', correct: false },
+            { text: '8', correct: false },
+        ]
+    },
+    {
+        question: 'What is 10 + 2?',
+        answers: [
+            { text: '12', correct: true },
+            { text: '22', correct: false },
+            { text: '14', correct: false },
+            { text: '43', correct: false }
+        ]
+    },
+    {
+        question: 'What is 12 + 2?',
+        answers: [
+            { text: '4', correct: false },
+            { text: '14', correct: true },
+            { text: '66', correct: false },
+            { text: '34', correct: false },
+        ]
+    },
+    {
+        question: 'What is 22 + 2?',
+        answers: [
+            { text: '9', correct: false },
+            { text: '2', correct: false },
+            { text: '5', correct: false },
+            { text: '24', correct: true }
         ]
     }
 ]
